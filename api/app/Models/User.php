24e52b0ad2af4a96f26ef5e6a1c824578cd0cb3c
zamelane <zamelane@vk.com>
2024-11-10 18:24:33 +0700

    Набросать авторизацию

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -2,24 +2,18 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
-        'email',
+        'login',
         'password',
     ];
 
@@ -41,7 +35,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
--- api/routes/api.php
+++ api/routes/api.php
@@ -1,4 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\AuthController;
 
-Route::get('/', fn() => 'Hello');
+Route::controller(AuthController::class)
+    ->group(function ($auth) {
+        $auth->post('login', 'login');
+    });
