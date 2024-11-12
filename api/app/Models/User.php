d9e195004637583ae15138ac86e63f096f429a37
zamelane <zamelane@vk.com>
2024-11-12 20:37:53 +0700

    Создать модели

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -19,22 +19,12 @@ class User extends Authenticatable
         'email',
         'birthDay'
     ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+    
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
