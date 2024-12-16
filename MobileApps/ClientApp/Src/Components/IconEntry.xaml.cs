a2e87081afed5675bd56ccf146c3ed7e17a25476
zamelane <zamelane@vk.com>
2024-12-16 19:28:49 +0700

    WIP

--- MobileApps/ClientApp/Src/Components/IconEntry.xaml.cs
+++ MobileApps/ClientApp/Src/Components/IconEntry.xaml.cs
@@ -2,23 +2,32 @@ namespace ClientApp.Src.Components;
 
 public partial class IconEntry : ContentView
 {
-	public IconEntry()
-	{
-		InitializeComponent();
-	}
+    public IconEntry()
+    {
+        InitializeComponent();
+    }
 
-	
-	public static readonly BindableProperty PlaceholderTextProperty = BindableProperty.Create(nameof(PlaceholderText), typeof(string), typeof(IconEntry), String.Empty);
-    public static readonly BindableProperty IconSourceProperty	    = BindableProperty.Create(nameof(IconSource), typeof(string), typeof(IconEntry), String.Empty);
 
-	public string PlaceholderText
-	{
-		get => (string)GetValue(PlaceholderTextProperty);
-		set => SetValue(PlaceholderTextProperty, value);
-	}
+    public static readonly BindableProperty PlaceholderTextProperty = BindableProperty.Create(nameof(PlaceholderText), typeof(string), typeof(IconEntry), String.Empty);
+    public static readonly BindableProperty IconSourceProperty = BindableProperty.Create(nameof(IconSource), typeof(string), typeof(IconEntry), String.Empty);
+    public static readonly BindableProperty TextProperty = BindableProperty.Create(nameof(Text), typeof(string), typeof(IconEntry), string.Empty);
+
+    public string PlaceholderText
+    {
+        get => (string)GetValue(PlaceholderTextProperty);
+        set => SetValue(PlaceholderTextProperty, value);
+    }
     public string IconSource
     {
         get => (string)GetValue(IconSourceProperty);
         set => SetValue(IconSourceProperty, value);
+    }
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set
+        {
+            SetValue(TextProperty, value);
+        }
     }
 }
--- MobileApps/ClientApp/Src/Components/TitledContentTemplate.xaml.cs
+++ MobileApps/ClientApp/Src/Components/TitledContentTemplate.xaml.cs
@@ -6,4 +6,17 @@ public partial class TitledContentTemplate : ContentView
 	{
 		InitializeComponent();
 	}
+
+    public static readonly BindableProperty TitleProperty = BindableProperty.Create(nameof(Title), typeof(string), typeof(TitledContentTemplate), String.Empty);
+    public static readonly BindableProperty ShowMoreProperty = BindableProperty.Create(nameof(ShowMore), typeof(bool), typeof(TitledContentTemplate), false);
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+    public bool ShowMore
+    {
+        get => (bool)GetValue(ShowMoreProperty);
+        set => SetValue(ShowMoreProperty, value);
+    }
 }
