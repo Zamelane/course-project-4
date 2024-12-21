eaf013c2a8911b45a51a8397f57bef74c5d211bd
zamelane <zamelane@vk.com>
2024-12-21 16:35:52 +0700

    Правки ТЗ Добавил regex валидацию логина при регистрации

--- api/app/Http/Requests/User/UserRegistrationRequest.php
+++ api/app/Http/Requests/User/UserRegistrationRequest.php
@@ -11,8 +11,8 @@ class UserRegistrationRequest extends ApiRequest
         return [
             'firstName' => 'required|string|min:2|max:75',
             'lastName'  => 'required|string|min:2|max:75',
-            'login'     => 'required|string|min:2|max:75|unique:users',
-            'password'  => 'required|string|min:2|max:75',
+            'login'     => 'required|string|min:2|regex:/^[a-zA-Z0-9_-]+$/|max:75|unique:users',
+            'password'  => 'required|string|min:8|max:75',
             'birthDay'  => 'required|date',
             'email'     => 'required|string|min:2|max:75|unique:users'
         ];
