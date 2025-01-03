64986e54d0a3e4967772d2be72d6e82879cb5ec7
zamelane <zamelane@vk.com>
2025-01-03 18:33:17 +0700

    Добавил страницу категорий, но нужно доработать

--- MobileApps/ClientApp/Src/ViewModels/CategoriesViewModel.cs
+++ MobileApps/ClientApp/Src/ViewModels/CategoriesViewModel.cs
@@ -1,7 +1,59 @@
-﻿namespace ClientApp.Src.ViewModels
-{
-    class CategoriesViewModel
-    {
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RequestsLibrary.Responses.Api.Category;
+using RequestsLibrary.Routes.API;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 
+namespace ClientApp.Src.ViewModels
+{
+    public partial class CategoriesViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<CategoryResponse>? categories;
+        [ObservableProperty]
+        private string? error = null;
+        [ObservableProperty]
+        private bool isFetching = false;
+
+        public CategoriesViewModel()
+        {
+            TryFetchCategories();
+        }
+
+        [RelayCommand]
+        private async Task TryFetchCategories()
+        {
+            try
+            {
+                IsFetching = true;
+                Debug.WriteLine("Start TryFetchCategories"); // TODO: DEBUG
+                (var response, var body, var exception) = await RequestsLibrary.Routes.API.Categories.GetAllRequest.RequestToServer();
+
+                if (body is null && exception is null)
+                    exception = new RequestsLibrary.Exceptions.RequestException("Сервер не вернул данные: " + response.StatusCode);
+
+                if (exception is not null)
+                {
+                    Error = exception.Message;
+                    Debug.WriteLine("Error TryFetchCategories: " + Error); // TODO: DEBUG
+                    return;
+                }
+
+                foreach (CategoryResponse cr in body!)
+                {
+                    cr.AccentColor = $"#{cr.AccentColor}";
+                    cr.BackgroundColor = $"#{cr.BackgroundColor}";
+                }
+
+                Categories = body;
+
+                Debug.WriteLine("End TryFetchCategories"); // TODO: DEBUG
+            }
+            finally
+            {
+                IsFetching = false;
+            }
+        }
     }
 }
--- MobileApps/RequestsLibrary/Responses/Api/Category/CategoryResponse.cs
+++ MobileApps/RequestsLibrary/Responses/Api/Category/CategoryResponse.cs
@@ -7,6 +7,6 @@ public class CategoryResponse : ErrorResponse
     [JsonPropertyName("id")] public int Id { get; set; }
     [JsonPropertyName("name")] public string? Name { get; set; }
     [JsonPropertyName("accent_color")] public string? AccentColor { get; set; }
-    [JsonPropertyName("backgroud_color")] public DateTime BackgroundColor { get; set; }
+    [JsonPropertyName("background_color")] public string? BackgroundColor { get; set; }
     [JsonPropertyName("image")] public CategoryImageModel? Image { get; set; }
 }
